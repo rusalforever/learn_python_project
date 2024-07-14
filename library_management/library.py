05c064e101892539aa309101f930609ce26d630f
Revan190 <programmer23w@gmail.com>
2024-07-14 20:52:34 +0200

    Edit library.py and create comics.py

--- library_management/library.py
+++ library_management/library.py
@@ -1,6 +1,7 @@
 from typing import List, Generator
 import logging
 from .book import Book
+from .comics import Comics
 
 logging.basicConfig(level=logging.INFO)
 
@@ -20,6 +21,16 @@ def check_book_exists(func):
 class Library:
     def __init__(self):
         self._books: List[Book] = []
+        self.initialize_starter_books()
+
+    def initialize_starter_books(self):
+        starter_books = [
+            Book(title="Кайдашева сім'я", author="Іван Семенович Нечуй-Левицький"),
+            Book(title="Лісова пісня", author="Леся Українка"),
+            Comics(title="Star Wars Knights of the Old Republic 43: The Reaping, Part 1", author="John Jackson Miller", illustrator="Dustin Weaver", series="Star Wars: Knights of the Old Republic"),
+            Comics(title="Unseen, Unheard", author="Chris Avellone", illustrator="Dustin Weaver", series="Star Wars Tales")
+        ]
+        self._books.extend(starter_books)
 
     @log_addition
     def add_book(self, book: Book):
